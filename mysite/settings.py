8c470c1cec0f2a1200ec474a8a17ec75dbb9a5ab
SaynProg <u56179410@gmail.com>
2022-07-22 13:22:56 +0300

    Second fixed settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'pj2m3157=2m7yh+wt#wd8ol5$!%)_u$eb#k+0i-!av4)uasr00'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saynsite.pythonanywhere.com',]
 
 
 # Application definition
@@ -88,8 +88,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+MEDIA_ROOT = '/home/saynsite/DjangoCMSProject/media'
+STATIC_ROOT = '/home/saynsite/DjangoCMSProject/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite', 'static'),
@@ -243,7 +243,7 @@ DATABASES = {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
         'HOST': 'localhost',
-        'NAME': 'project.db',
+        'NAME': '/home/saynsite/DjangoCMSProject/project.db',
         'PASSWORD': '',
         'PORT': '',
         'USER': ''
@@ -256,7 +256,7 @@ THUMBNAIL_PROCESSORS = (
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters'
 )
-META_SITE_PROTOCOL = 'http'  # set 'http' for non ssl enabled websites
+META_SITE_PROTOCOL = 'https'  # set 'http' for non ssl enabled websites
 META_USE_SITES = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
